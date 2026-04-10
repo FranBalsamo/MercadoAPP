80e5fcadafba64e7543af76429c1052780c05d9e
FranBalsamo <balsamof.11@gmail.com>
2026-04-10 17:45:21 -0300

    Fix: Se arreglo el bug de calculo de subtotal y total de una boleta nueva

--- backend/src/main/java/com/franbalsamo/mercadoapp/Service/BoletaService.java
+++ backend/src/main/java/com/franbalsamo/mercadoapp/Service/BoletaService.java
@@ -54,7 +54,9 @@ public class BoletaService {
             nuevaVenta.setCantidad(vDto.getCantidad());
             nuevaVenta.setPrecio_unitario(vDto.getPrecio_unitario());
             nuevaVenta.setPrecio_vacio(vDto.getPrecio_vacio());
-            nuevaVenta.setSubtotal((vDto.getCantidad() * vDto.getPrecio_unitario() + ((int)vDto.getCantidad())) * vDto.getPrecio_vacio());
+
+            float subtotal = (vDto.getCantidad() * vDto.getPrecio_unitario()) + (((int)vDto.getCantidad()) * vDto.getPrecio_vacio());
+            nuevaVenta.setSubtotal(subtotal);
 
             nuevaBoleta.addVenta(nuevaVenta);
             totalCalculado += nuevaVenta.getSubtotal();
@@ -90,7 +92,9 @@ public class BoletaService {
                 ventaEncontrada.setCantidad(vDto.getCantidad());
                 ventaEncontrada.setPrecio_unitario(vDto.getPrecio_unitario());
                 ventaEncontrada.setPrecio_vacio(vDto.getPrecio_vacio());
-                ventaEncontrada.setSubtotal((vDto.getCantidad() * vDto.getPrecio_unitario() + ((int)vDto.getCantidad())) * vDto.getPrecio_vacio());
+
+                float subtotal = (vDto.getCantidad() * vDto.getPrecio_unitario()) + (((int)vDto.getCantidad()) * vDto.getPrecio_vacio());
+                ventaEncontrada.setSubtotal(subtotal);
 
                 ventasActuales.remove(ventaEncontrada);
 
@@ -102,7 +106,8 @@ public class BoletaService {
                 nuevaVenta.setCantidad(vDto.getCantidad());
                 nuevaVenta.setPrecio_unitario(vDto.getPrecio_unitario());
                 nuevaVenta.setPrecio_vacio(vDto.getPrecio_vacio());
-                nuevaVenta.setSubtotal((vDto.getCantidad() * vDto.getPrecio_unitario() + ((int)vDto.getCantidad())) * vDto.getPrecio_vacio());
+                float subtotal = (vDto.getCantidad() * vDto.getPrecio_unitario()) + (((int)vDto.getCantidad()) * vDto.getPrecio_vacio());
+                nuevaVenta.setSubtotal(subtotal);
                 
                 boleta.addVenta(nuevaVenta);
 
